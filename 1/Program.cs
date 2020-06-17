85370e1c2a6f5c8bc1922f1600c5a6d54415b9ca
viprom4 <123a56z5@gmail.com>
2020-06-17 09:50:23 +0300

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime time1 =new DateTime(2019, 12, 05, 09, 53, 00);
+            Console.WriteLine(time1);
+
+            while (time1>DateTime.Now)
+            {
+                Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine(DateTime.Now);
+            }
+
+            Console.Beep();
+            Console.ReadKey();
+
+        }
+    }
+}
